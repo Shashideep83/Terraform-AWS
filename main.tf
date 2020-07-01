a889538c695572843f89e9f0069a939d3cd57621
Shashi deep <shashidulam83@gmail.com>
2020-07-01 20:19:50 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,33 @@
+resource "aws_cloudwatch_metric_alarm" "cpu-utilization" {
+  alarm_name          = "high-cpu-utilization-alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "75"
+  alarm_description   = "cpu-utilization exceeded 75 %"
+  alarm_actions       = ["${var.sns_topic}"]
+
+  dimensions = {
+    InstanceId = "${var.instance_id}"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "instance-health-check" {
+  alarm_name          = "instance-status-check"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "StatusCheckFailed"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "1"
+  alarm_description   = "status check failed, server is not responding"
+  alarm_actions       = ["${var.sns_topic}"]
+
+  dimensions = {
+    InstanceId = "${var.instance_id}"
+  }
+}
