9370e69ad9c079e0f5896dc4d4d150f98fb95b69
Shashi deep <shashidulam83@gmail.com>
2020-07-01 20:17:12 +0530

    Create main.tf

---
+++ s3/main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "random_id" "my-random-id" {
+  byte_length = 2
+}
+
+resource "aws_s3_bucket" "my-test-bucket" {
+  bucket = "${var.s3_bucket_name}-${random_id.my-random-id.dec}"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    enabled = true
+
+    transition {
+      storage_class = "STANDARD_IA"
+      days          = 30
+    }
+  }
+
+  tags = {
+    Name = "test"
+  }
+}
