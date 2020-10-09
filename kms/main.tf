8a67a6bdc00f870d479e6f9c6c2caece370f6413
Shashi deep <shashidulam83@gmail.com>
2020-10-09 23:51:07 +0530

    Update main.tf

--- kms/main.tf
+++ kms/main.tf
@@ -13,7 +13,7 @@ resource "aws_kms_key" "mykesy" {
   policy = <<EOF
 {
     "Id": "key-consolepolicy-3",
-    "Version": "2012-10-17",
+    "Version": "20120-10-17",
     "Statement": [
         {
             "Sid": "Enable IAM User Permissions",
